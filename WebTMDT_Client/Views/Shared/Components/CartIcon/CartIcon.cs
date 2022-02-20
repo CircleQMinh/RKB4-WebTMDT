e3c3506205153f623f001c8473e5dd80f2c692e9
CircleQMinh <quocminh.vutran3105@gmail.com>
2022-02-20 11:05:32 +0700

    Add Cart Function to project

---
+++ WebTMDT_Client/Views/Shared/Components/CartIcon/CartIcon.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebTMDTLibrary.DTO;
+
+namespace WebTMDT_Client.Views.Shared.Components.CartIcon
+{
+    [ViewComponent]
+    public class CartIcon : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var session = HttpContext.Session;
+
+            var cart_str = session.GetString("cart");
+            if (cart_str == null)
+            {
+             
+                ViewBag.TotalItem = 0;
+            }
+            else
+            {
+                Cart cart = JsonConvert.DeserializeObject<Cart>(cart_str);
+                ViewBag.TotalItem = cart.TotalItem;
+            }
+            return View("CartIcon");
+        }
+    }
+}
